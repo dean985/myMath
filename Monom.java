1d57d7fe896e16a28d5f3fb6be240b46337483b7
Dean <carmidean@gmail.com>
2019-10-29 19:46:37 +0200

    d-ver 1

--- Monom.java
+++ Monom.java
@@ -14,8 +14,25 @@ public class Monom implements function{
 	public static final Monom ZERO = new Monom(0,0);
 	public static final Monom MINUS1 = new Monom(-1,0);
 	public static final double EPSILON = 0.0000001;
+	
+	private double _coefficient; 
+	private int _power;
 	public static final Comparator<Monom> _Comp = new Monom_Comperator();
-	public static Comparator<Monom> getComp() {return _Comp;}
+
+	private static Monom getNewZeroMonom() {
+		return new Monom(ZERO);
+	}
+	public static Comparator<Monom> getComp() {
+		return _Comp;
+	}
+	private void set_coefficient(double a){
+		this._coefficient = a;
+	}
+	private void set_power(int p) {
+		if(p<0) {throw new RuntimeException("ERR the power of Monom should not be negative, got: "+p);}
+		this._power = p;
+	}
+
 	public Monom(double a, int b){
 		this.set_coefficient(a);
 		this.set_power(b);
@@ -35,7 +52,9 @@ public class Monom implements function{
 	 * @return
 	 */
 	public Monom derivative() {
-		if(this.get_power()==0) {return getNewZeroMonom();}
+		if(this.get_power()==0) {
+			return getNewZeroMonom();
+		}
 		return new Monom(this.get_coefficient()*this.get_power(), this.get_power()-1);
 	}
 	public double f(double x) {
@@ -44,9 +63,37 @@ public class Monom implements function{
 		ans = this.get_coefficient()*Math.pow(x, p);
 		return ans;
 	} 
-	public boolean isZero() {return this.get_coefficient() == 0;}
+	public boolean isZero() {
+		return this.get_coefficient() == 0;
+	}
 	// ***************** add your code below **********************
-	public Monom(String s) {;}
+	public Monom(String s) {
+		if(s.length()>0){
+			if (s.contains("x")){
+				//Coefficient
+				String coeff = s.substring(0,s.indexOf("x"));
+				double coeffD = Double.parseDouble(coeff);
+				this.set_coefficient(coeffD);
+				//Power
+				if(s.contains("^")){
+					int indexP = s.indexOf("^");
+					Double p = Double.parseDouble(s.substring(indexP));
+				}
+				else{
+					this.set_power(1);
+				}
+
+			}
+			else{
+				double sDouble = Double.parseDouble(s);
+				this.set_coefficient(sDouble);
+				this.set_power(0);
+			}
+			 
+			
+		}
+		
+	}
 	
 	public void add(Monom m) {;}
 	
@@ -61,16 +108,8 @@ public class Monom implements function{
 	//****************** Private Methods and Data *****************
 	
 
-	private void set_coefficient(double a){
-		this._coefficient = a;
-	}
-	private void set_power(int p) {
-		if(p<0) {throw new RuntimeException("ERR the power of Monom should not be negative, got: "+p);}
-		this._power = p;
-	}
-	private static Monom getNewZeroMonom() {return new Monom(ZERO);}
-	private double _coefficient; 
-	private int _power;
+	
+	
 	
 	
 }
