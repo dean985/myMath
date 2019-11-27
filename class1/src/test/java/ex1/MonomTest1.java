6222cf8ee72f18806835e552ac80d794cd23f636
Dean <carmidean@gmail.com>
2019-11-27 09:56:28 +0200

    init - branch creation

---
+++ class1/src/test/java/ex1/MonomTest1.java
@@ -0,0 +1,60 @@
+package ex1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonomTest1 {
+
+    @Test
+    void get_coefficient() {
+        String[] monoms = {"-2x", "0", "x","-x^3","-1x^5","x^6","x^-1","-x^0"};
+        for(int i =0; i<monoms.length; i++){
+            Monom m = new Monom("")
+        }
+    }
+
+    @Test
+    void get_power() {
+    }
+
+    @Test
+    void getComp() {
+    }
+
+    @Test
+    void derivative() {
+    }
+
+    @Test
+    void f() {
+    }
+
+    @Test
+    void isZero() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void initFromString() {
+    }
+
+    @Test
+    void copy() {
+    }
+}
