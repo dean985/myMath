7812fd9465692cfa873da8457e31d5e718cf7d19
elonezra <elonezra1995@hotmail.com>
2019-12-11 21:08:02 +0200

    finished the Functions_GUI wait to change to use the ComplexFunction

--- class1/src/main/java/ex1/Functions_GUI.java
+++ class1/src/main/java/ex1/Functions_GUI.java
@@ -1,41 +1,96 @@
 package ex1;
 
+
 import java.awt.*;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.function.Function;
+
+//import javax.json.Json;
+//import javax.json.JsonArray;
+//import javax.json.JsonObject;
+//import javax.json.JsonReader;
+//import javax.json.JsonValue;
+//import javax.json.stream.JsonParser;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Functions_GUI implements functions {
 
     public static Color[] Colors = {Color.blue, Color.cyan, Color.MAGENTA, Color.ORANGE,
             Color.red, Color.GREEN, Color.PINK};
-    static ArrayList<function> ff = new ArrayList<function>();
+    ArrayList<function> ff = new ArrayList<function>();
+//
+//      Functions_GUI()
+//      {
+//          super();
+//      }
 
-    public static ArrayList<function> init() {
-        ArrayList<function> ans = new ArrayList<function>();
-        Polynom s0 = new Polynom("x^2+x+2");
-        Polynom s1 = new Polynom("4x+1");
-        Polynom s2 = new Polynom("0.3x^3");
-        ans.add(s0); ans.add(s1); ans.add(s2);
-        return ans;
-    }
 
     public static void main(String[] args) {
 
-            ff.addAll(init());
-            Functions_GUI functions_gui = new Functions_GUI();
-            functions_gui.drawFunctions(800,600,new Range(-10,10),new Range(-1.2,1.5),200);
+            //ff.addAll(init());
+       Functions_GUI functions_gui = new Functions_GUI();
+
+        try {
+            functions_gui.initFromFile("/home/elonezra/IdeaProjects/myMath/myMath/myMath/class1/function_file.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        functions_gui.drawFunctions("/home/elonezra/IdeaProjects/myMath/myMath/myMath/class1/src/main/GUI_params.txt");
+      }
+
+    @Override
+    public void initFromFile(String file) throws IOException
+    {
+
+        try {
+           FileInputStream streamIn = new FileInputStream(file);
+           BufferedReader reader = new BufferedReader(new InputStreamReader(streamIn));
+           String line = reader.readLine();
+            ff.add(new Polynom("x^2"));
+            ff.add(new Polynom("x^3+4x^2+1"));
+
+            while(line != null)
+            {
+                System.out.println(line);
+
+               // spoused to have ff.add(new Complexfunction(Function.initFromString(line))
+                line = reader.readLine();
+            }
+
+            reader.close();
+            streamIn.close();
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
-    public void initFromFile(String file) throws IOException {
+    public void saveToFile(String file) throws IOException
+    {
 
-    }
+        ObjectOutputStream oos;
+        try {
+            FileOutputStream fout = new FileOutputStream(file, true);
+            oos = new ObjectOutputStream(fout);
+            oos.writeObject(ff);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    @Override
-    public void saveToFile(String file) throws IOException {
+
 
     }
 
@@ -65,65 +120,115 @@ public class Functions_GUI implements functions {
 
         //////// vertical lines
         StdDraw.setPenColor(Color.LIGHT_GRAY);
-        for (int i = 0; i <= n; i=i+10) {
-            StdDraw.line(x[i], ry.get_min(), x[i], ry.get_max());
+        for (int i = (int) rx.get_min(); i <= rx.get_max(); i=i+1) {
+            StdDraw.line(i,ry.get_min(), i, ry.get_max());
         }
+
 
         //////// horizontal  lines
         for (double i = ry.get_min(); i <= ry.get_max(); i=i+0.5) {
-            StdDraw.line(rx.get_min(), i,rx.get_max(), i);
+            StdDraw.line(rx.get_min(), (i),rx.get_max(), (i));
         }
 
         //////// x axis
         StdDraw.setPenColor(Color.BLACK);
         StdDraw.setPenRadius(0.005);
 
-        StdDraw.line(0, 0, height/2, 0);
-        StdDraw.setFont(new Font("TimesRoman", Font.BOLD, 10));
-        for (int i = 0; i <= n; i=i+10) {
-            StdDraw.text(x[i]-0.07, -0.07, Integer.toString(i-n/2));
+        StdDraw.line(rx.get_min(), 0, rx.get_max(), 0);
+        StdDraw.setFont(new Font("--diff3-cmd CMD", Font.BOLD, 10));
+
+
+        for (int i = (int)rx.get_min()+1; i < rx.get_max() ; i++) {
+                StdDraw.line(i, -0.02, i, 0.02);
+                StdDraw.text(i, -0.07, i+"");
         }
 
         //////// y axis
-        StdDraw.line(x[n/2], ry.get_min(), x[n/2], ry.get_max());
-        for (double i = ry.get_min(); i <= ry.get_max(); i=i+0.5) {
-            StdDraw.text(x[n/2]-0.07, i+0.07, Double.toString(i));
+        StdDraw.line(0, ry.get_min(), 0, ry.get_max());
+
+
+
+        for (int i = (int)ry.get_min(); i < ry.get_max(); i++) {
+            if(i != 0){
+            StdDraw.line(-0.2, i, 0.2, i);
+            StdDraw.text(0.5, i, i+"");}
         }
 
+
+
         // plot the approximation to the function
-//        for(int a=0;a<size;a++) {
-//            int c = a%Colors.length;
-//            StdDraw.setPenColor(Colors[c]);
-//            System.out.println( a +") "+ Colors[a] + "  f(x)= " + ff.get(a));
-//            for (int i = 0; i < n; i++) {
-//                StdDraw.line(x[i], yy[a][i], x[i+1], yy[a][i+1]);
-//            }
-//        }
+        for(int a=0;a<size;a++) {
+            int c = a%Colors.length;
+            StdDraw.setPenColor(Colors[c]);
+            System.out.println( a +") "+ Colors[a] + "  f(x)= " + ff.get(a));
+            for (int i = 0; i < n; i++) {
+                StdDraw.line(x[i], yy[a][i], x[i+1], yy[a][i+1]);
+            }
+        }
 
     }
 
     @Override
     public void drawFunctions(String json_file)
-    { ; }
+    {
 
+
+
+        JSONObject json  = new JSONObject(loadJSONFromAsset(json_file));
+
+         int W=800, H=600, RES=100;
+        W = json.getInt("Width");
+        H = json.getInt("Height");
+        RES = json.getInt("Resolution");
+        Range RX = new Range(json.getJSONArray("Range_X").getDouble(0),json.getJSONArray("Range_X").getDouble(1));
+        Range RY = new Range(json.getJSONArray("Range_Y").getDouble(0),json.getJSONArray("Range_Y").getDouble(1));
+
+        drawFunctions(W,H, RX, RY, RES);
+
+
+    }
+
+
+    public static String loadJSONFromAsset( String path)
+    {
+        String json = null;
+        try {
+
+            InputStream is = null;
+            try {
+                is = new FileInputStream(path);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+
+            return null;
+        }
+        return json;
+    }
 
     @Override
     public int size() {
-        return 0;
+        return ff.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return ff.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return ff.contains(o);
     }
 
     @Override
     public Iterator<functions> iterator() {
+        functions iterator = (functions) this.ff.iterator();
         return null;
     }
 
@@ -134,41 +239,49 @@ public class Functions_GUI implements functions {
 
     @Override
     public <T> T[] toArray(T[] ts) {
-        return null;
+        return this.ff.toArray(ts);
     }
 
     @Override
     public boolean add(functions functions) {
-        return false;
+        return ff.add((function) functions);
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        return ff.remove(o);
     }
 
     @Override
     public boolean containsAll(Collection<?> collection) {
-        return false;
+        return this.ff.containsAll(collection);
     }
 
     @Override
-    public boolean addAll(Collection<? extends functions> collection) {
-        return false;
+    public boolean addAll(Collection<? extends functions> collection)
+    {
+
+        return this.ff.addAll((Collection<? extends function>) collection);
     }
 
     @Override
     public boolean removeAll(Collection<?> collection) {
-        return false;
+        return this.ff.removeAll(collection);
     }
 
     @Override
     public boolean retainAll(Collection<?> collection) {
-        return false;
+        return this.ff.retainAll(collection);
     }
 
     @Override
     public void clear() {
 
+        this.ff.clear();
+
     }
+
+
+
+
 }
